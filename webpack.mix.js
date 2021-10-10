32a55f3b7f26e12f7d4db361e49a9c81da9b8651
maroqijalil <jalilpro31@gmail.com>
2021-10-11 06:24:55 +0700

    feat: add tailwind css template

--- webpack.mix.js
+++ webpack.mix.js
@@ -12,6 +12,11 @@ const mix = require('laravel-mix');
  */
 
 mix.js('resources/js/app.js', 'public/js')
-    .postCss('resources/css/app.css', 'public/css', [
-        //
-    ]);
+	.postCss('resources/css/app.css', 'public/css', [
+			require("tailwindcss"),
+			require("autoprefixer"),
+			require("cssnano")({
+				preset: 'default'
+			})
+		]
+	);
